08676400e6b715a5de08b4fc99baddf8ebfd56ed
Richard H. <richard@haddads.net.au>
2024-02-24 17:17:11 +1000

    fix eslint

--- src/app/(server-components)/SectionHero.tsx
+++ src/app/(server-components)/SectionHero.tsx
@@ -15,12 +15,18 @@ const SectionHero: FC<SectionHeroProps> = ({ className = "" }) => {
     <div
       className={`nc-SectionHero flex flex-col-reverse lg:flex-col relative ${className}`}
     >
-      <div className="flex flex-col lg:flex-row lg:items-center">
-        <div className="flex-shrink-0 lg:w-1/2 flex flex-col items-start space-y-8 sm:space-y-10 pb-14 lg:pb-64 xl:pr-14 lg:mr-10 xl:mr-0">
-          <h2 className="font-medium text-4xl md:text-5xl xl:text-7xl !leading-[114%] ">
+      <div className={`flex flex-col lg:flex-row lg:items-center`}>
+        <div
+          className={`flex-shrink-0 lg:w-1/2 flex flex-col items-start space-y-8 sm:space-y-10 pb-14 lg:pb-64 xl:pr-14 lg:mr-10 xl:mr-0`}
+        >
+          <h2
+            className={`font-medium text-4xl md:text-5xl xl:text-7xl !leading-[114%]`}
+          >
             Australia's own holiday guru
           </h2>
-          <span className="text-base md:text-lg text-neutral-500 dark:text-neutral-400">
+          <span
+            className={`text-base md:text-lg text-neutral-500 dark:text-neutral-400`}
+          >
             Discover Australia's hidden gems and breathtaking retreats with Book
             It Mate, your gateway to unforgettable holiday experiences
           </span>
@@ -29,11 +35,11 @@ const SectionHero: FC<SectionHeroProps> = ({ className = "" }) => {
           </ButtonPrimary>
         </div>
         <div className="flex-grow">
-          <Image className="w-full" src={imagePng} alt="hero" priority />
+          <Image className={`w-full`} src={imagePng} alt="hero" priority />
         </div>
       </div>
 
-      <div className="hidden lg:block z-10 mb-12 lg:mb-0 lg:-mt-52 w-full">
+      <div className={`hidden lg:block z-10 mb-12 lg:mb-0 lg:-mt-52 w-full`}>
         <HeroSearchForm />
       </div>
     </div>
